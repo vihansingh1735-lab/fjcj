43e08e8d9444d40f6240618802cc38a2bf0e4f26
istealyourhome <vihansingh1735@gmail.com>
2026-01-18 11:30:06 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 const guildId = new URLSearchParams(location.search).get("guildId");
-
+const API_BASE = "http://2.56.246.81:30085";
 fetch(`https://YOUR_ORIHOST_IP:PORT/api/premium?guildId=${guildId}`)
   .then(r=>r.json())
   .then(d=>{
